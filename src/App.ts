8cbf4599ce1faade0f7d81c4c2c7f61cd0b5f2e9
Patrício Andrade <tricioandrade@outlook.com>
2022-11-16 16:02:15 +0100

    update

--- src/App.ts
+++ src/App.ts
@@ -1,10 +1,14 @@
 import 'bootstrap';
-
+import '@fortawesome/fontawesome/css/fontawesome';
+import '@fortawesome/fontawesome/css/fontawesome.css';
+// import '@fortawesome/fontawesome-free';
 
 
 
 const body: HTMLElement = document.querySelector('body')!;
-body.innerHTML = "<h1 class='fa fa-home'></h1> " ;
+body.innerHTML = "<i class='fa-solid fa-home'></i>"   
++
+`<h1 class='btn-primary'>ola</h1>`;
 
 // console.log(fontawesome);
 ;
